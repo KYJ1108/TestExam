064838456f1ceff1beada7439a59332d28cea195
unknown <kimyeji546408@gmail.com>
2024-03-15 15:37:35 +0900

    test2

--- src/main/java/org/example/Exam2.java
+++ src/main/java/org/example/Exam2.java
@@ -11,12 +11,15 @@ public class Exam2 {
          *****
 
          */
+
         for (int i=1; i<4; i++){
-            for (int j=2; j>=0; j--){
+            for (int j=2; j>=0; j--){ // 널어디 둬야하니...
                 System.out.print(" ");
             }
             for (int c=1; c<=i; c++){
-                System.out.print("*");
+                for (int d=c; d<=i; d++){
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
@@ -34,6 +37,17 @@ public class Exam2 {
          *********
 
          */
+        for (int i=1; i<6; i++){
+            for (int j=5; j>=0; j--){
+                System.out.print(" ");
+            }
+            for (int c=1; c<=i; c++){
+                for (int d=c; d<=i; d++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 
         // 높이 : 7
 
@@ -50,6 +64,17 @@ public class Exam2 {
          *************
 
          */
+        for (int i=1; i<8; i++){
+            for (int j=7; j>=0; j--){
+                System.out.print(" ");
+            }
+            for (int c=1; c<=i; c++){
+                for (int d=c; d<=i; d++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
